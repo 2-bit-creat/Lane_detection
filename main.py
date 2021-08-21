6b698e15b1d49e6db3f031d162564df365186421
2-bit-creat <81176520+2-bit-creat@users.noreply.github.com>
2021-08-21 19:48:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,14 +8,10 @@ from set_encoder import writeBuffer, serWrite
 
 class LaneDetection:
 
-    low_yellow = np.array([15, 150, 120])
+    low_yellow = np.array([15, 150, 120]) #hsv
     high_yellow = np.array([25, 230, 210])
-    low_white = np.array([0, 0, 70])
-    high_white = np.array([3, 60, 230])
-    #low_white_hls = np.array([0, 153, 0])
-    #high_white_hls = np.array([22, 179, 13])
-    # low_white_rgb = np.array([160, 140, 140])
-    # high_white_rgb = np.array([175, 170, 170])
+    low_white = np.array([0, 150, 0]) #hsl
+    high_white = np.array([255, 255, 255])
 
     def run(self, vid):
         cap = cv2.VideoCapture(vid)
@@ -25,41 +21,57 @@ class LaneDetection:
             colored_image = self.color_detect(frame)
             cropped_image = self.cropping_image(colored_image)
             bird_eye_image = self.bird_eye(cropped_image)
-            slidng_list_left, sliding_list_right = self.sliding(bird_eye_image) #return two lists comprised of tuples
-            frame_steer = self.steering(frame, slidng_list_left, sliding_list_right) #print steering direction
+            self.slidng_list_left = self.sliding_left(bird_eye_image) #return list of tuples(avg points of left side)
+            self.sliding_list_right = self.sliding_right(bird_eye_image) #return list of tuples(avg points of right side)
+            frame_steer = self.steering(frame) #print steering value
 
             bird_eye_image = cv2.cvtColor(bird_eye_image, cv2.COLOR_GRAY2BGR)
-            for i in slidng_list_left:
+            for i in self.slidng_list_left:
                 cv2.circle(bird_eye_image, i, 5, (255, 255, 0), -1)
-            for i in sliding_list_right:
+            for i in self.sliding_list_right:
                 cv2.circle(bird_eye_image, i, 5, (255, 255, 0), -1)
 
-            cv2.imshow("bird_eye", cropped_image)
+            cv2.imshow("bird_eye", bird_eye_image)
             cv2.imshow("frame", frame_steer)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
         cap.release()
         cv2.destroyAllWindows()
 
-    def steering(self, frame, sliding_left, sliding_right):
+    def steering(self, frame):
         x_left = []
         x_right = []
-
-        for i in range(0, len(sliding_left)): #extract x coordinate
-            x_left.append(sliding_left[i][0])
+        for i in range(0, len(self.slidng_list_left)): #extract x coordinate of left side
+            x_left.append(self.slidng_list_left[i][0])
         left_arr = np.array(x_left)
+        left_diff_arr = np.diff(x_left)
+        left_diff_sum = np.sum(left_diff_arr)
+        left_avg = int(left_diff_sum / 5) #avg slope values of left side
 
-        for i in range(0, len(sliding_right)): #extract x coordinate
-            x_right.append(sliding_right[i][0])
+        for i in range(0, len(left_diff_arr)):
+            if (left_diff_arr[i] > (left_avg +10)) or (left_diff_arr[i] < (left_avg - 10)): # remove inappropriate cases
+                self.slidng_list_left = []
+                left_diff_sum = 0
+
+        for i in range(0, len(self.sliding_list_right)): #extract x coordinate of right side
+            x_right.append(self.sliding_list_right[i][0])
         right_arr = np.array(x_right)
+        right_diff_arr = np.diff(x_right)
+        right_diff_sum = np.sum(right_diff_arr)
+        right_avg = int(right_diff_sum / 5) #avg slope values of right side
 
-        avg_val = int((np.sum(np.diff(x_left))+np.sum(np.diff(x_right)))/2)
+        for i in range(0, len(right_diff_arr)):
+            if (right_diff_arr[i] > (right_avg +10)) or (right_diff_arr[i] < (right_avg - 10)): # remove inappropriate cases
+                self.sliding_list_right = []
+                right_diff_sum = 0
+
+        avg_val = int((left_diff_sum + right_diff_sum)/2) #avg of the avg slope valuses (dx/dy) of each side
         print(avg_val)
-        if np.sum(avg_val) < -7:
+        if np.sum(avg_val) < -7: #turn right
             cv2.arrowedLine(frame, (300, 340), (340, 340), (255, 0, 0), 4)
-        elif np.sum(avg_val) > 7:
+        elif np.sum(avg_val) > 7: #turn left
             cv2.arrowedLine(frame, (340, 340), (300, 340), (255, 0, 0), 4)
-        else:
+        else: #go straight
             cv2.arrowedLine(frame, (320, 340), (320, 300), (255, 0, 0), 4)
 
         return frame
@@ -76,28 +88,53 @@ class LaneDetection:
                     if cv2.waitKey(1) & 0xFF == ord('q'):
                         break
 
-    def sliding(slef, img):
+    def sliding_left(self, img):
         left_list = []
+        for j in range(259, img.shape[0] - 20, 40): #row
+            j_list = []
+            for i in range(19, int(img.shape[1]/2) - 20, 5): #col left side
+                num_sum = np.sum(img[j - 19:j + 21, i - 19:i + 21]) #window size is 20*20
+                if num_sum > 100000: #pick i given j where its num_sum is over 100000
+                    j_list.append(i)
+            try:
+                len_list = []
+                result = np.split(j_list, np.where(np.diff(j_list) > 5)[0] + 1) #cluster in case a gap b/w elements is over 5
+
+                for k in range(0, len(result)):
+                    len_list.append(len(result[k]))
+                largest_integer = max(len_list)
+
+                for l in range(0, len(result)):
+                    if len(result[l]) == largest_integer:
+                        avg = int(np.sum(result[l]) / len(result[l]))  # average
+                        left_list.append((avg, j))  # avg points of left side
+            except:
+                continue
+        return left_list
+
+    def sliding_right(self, img):
         right_list = []
         for j in range(259, img.shape[0] - 20, 40): #row
             j_list = []
-            for i in range(19, img.shape[1] - 20, 5): #col
-                num_sum = np.sum(img[j - 9:j + 11, i - 9:i + 11]) #window size is (10,10)
-                if num_sum > 4000: #pick i given j where its num_sum is over 4000
+            for i in range(int(img.shape[1]/2), img.shape[1] - 20, 5): #col right side
+                num_sum = np.sum(img[j - 19:j + 21, i - 19:i + 21]) #window size is 20*20
+                if num_sum > 100000: #pick i given j where its num_sum is over 100000
                     j_list.append(i)
             try:
-                result = np.split(j_list, np.where(np.diff(j_list) > 5)[0] + 1) #clustering
+                len_list = []
+                result = np.split(j_list, np.where(np.diff(j_list) > 5)[0] + 1) #cluster in case a gap b/w elements is over 5
+
                 for k in range(0, len(result)):
-                    if len(result[k]) < 5:
-                        continue
-                    avg = int(np.sum(result[k]) / len(result[k])) #average
-                    if avg < 320:
-                        left_list.append((avg, j)) #avg points of left side
-                    else:
-                        right_list.append((avg, j)) #avg points of right side
+                    len_list.append(len(result[k]))
+                largest_integer = max(len_list)
+
+                for l in range(0, len(result)):
+                    if len(result[l]) == largest_integer:
+                        avg = int(np.sum(result[l]) / len(result[l]))  # average
+                        right_list.append((avg, j))  # avg points of left side
             except:
                 continue
-        return left_list, right_list
+        return right_list
 
     def region_of_interest(self, img, vertices):
         mask = np.zeros_like(img)
@@ -120,18 +157,14 @@ class LaneDetection:
 
     def color_detect(self, img):
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        hls = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
         mask_yellow = cv2.inRange(hsv, self.low_yellow, self.high_yellow)
-        mask_white = cv2.inRange(hsv, self.low_white, self.high_white)
+        mask_white = cv2.inRange(hls, self.low_white, self.high_white)
         mask = cv2.bitwise_or(mask_white, mask_yellow)
         return mask
 
     def bird_eye(self, frame):
         frame = cv2.resize(frame, dsize=(640, 480), interpolation=cv2.INTER_AREA)
-        # cv2.circle(frame, (170, 339), 5, (0, 0, 255), -1)
-        # cv2.circle(frame, (420, 339), 5, (0, 0, 255), -1)
-        # cv2.circle(frame, (90, 419), 5, (0, 0, 255), -1)
-        # cv2.circle(frame, (490, 419), 5, (0, 0, 255), -1)
-        #cv2.imshow("frame", frame)
         pts1 = np.float32([[180, 339], [410, 339], [90, 419], [490, 419]])
         pts2 = np.float32([[0, 0], [640, 0], [0, 480], [640, 480]])
 
@@ -140,12 +173,6 @@ class LaneDetection:
         return result
 
 if __name__ == '__main__':
-    #seri = serial.Serial("COM3", 115200)
     a = LaneDetection()
     a.run('road_vid2_Trim.mp4')
-    # cap = cv2.VideoCapture('road_vid2_Trim.mp4')
-    # while True:
-    #     _, frame = cap.read()
-    #     cv2.imshow("bird", a.bird_eye(frame))
-    #     cv2.waitKey(1)
 
